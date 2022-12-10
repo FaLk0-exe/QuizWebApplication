d51d8e7fca41de4b9218ab0773546a2c199c7df2
Alex Falko <aprox512@gmail.com>
2022-12-10 14:18:28 +0200

    Добавьте файлы проекта.

---
+++ QuizWebApplication/Controllers/GoogleOauthController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using QuizWebApplication.Helpers;
+using QuizWebApplication.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QuizWebApplication.Controllers
+{
+    public class GoogleOauthController:Controller
+    {
+        private string _scope = "https://www.googleapis.com/auth/youtube";
+        private string _redirectUrl = "https://localhost:5001/GoogleOauth/Code";
+        public IActionResult RedirectOnOauthServer()
+        {
+            var codeVerifier = Guid.NewGuid().ToString();
+            var codeChallenge = Sha256Helper.ComputeHash(codeVerifier);
+            return Redirect(GoogleOauthService.GenerateOauthRequestUrl(_scope,_redirectUrl,codeChallenge));
+        }
+
+        public async Task<IActionResult> CodeAsync(string code)
+        {
+            string codeVerifier = HttpContext.Session.GetString("codeVerifier");
+            var tokenResult = await GoogleOauthService.ExchangeCodeOnTokenAsync(code,codeVerifier,_redirectUrl);
+            return Ok();
+        }
+    }
+}
---
+++ QuizWebApplication/Services/GoogleOauthService.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.WebUtilities;
+using QuizWebApplication.Helpers;
+using QuizWebApplication.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QuizWebApplication.Services
+{
+    public class GoogleOauthService
+    {
+        private const string _clientId = "628886548175-dvbc1vv5a1ii62p8a65kllun4dh0v30k.apps.googleusercontent.com";
+        private const string _clientSecret = "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";
+        private const string _refreshEndpoint = "https://oauth2.googleapis.com/token";
+        public static string GenerateOauthRequestUrl(string scope,string redirectUrl,string codeChallenge) 
+        {
+            var oauthServerEndpoint = "https://accounts.google.com/o/oauth2/v2/auth";
+            var queryParams = new Dictionary<string, string>()
+            {
+                {"client_id",_clientId},
+                {"redirect_uri",redirectUrl},
+                {"response_type","code"},
+                {"scope",scope },
+                { "code_challenge",codeChallenge},
+                { "code_challenge_method","S256" },
+                {"access_type","offline" }
+            };
+            var url = QueryHelpers.AddQueryString(oauthServerEndpoint, queryParams);
+            return url;
+        }
+        public static async Task<TokenResult> ExchangeCodeOnTokenAsync(string code, string codeVerifier, string redirectUrl) 
+        {
+            var tokenEndpoint = "https://oauth2.googleapis.com/token";
+            var oauthParams = new Dictionary<string, string>()
+            {
+                { "client_id",_clientId},
+                { "client_secret",_clientSecret},
+                {"code",code },
+                { "code_verifier",codeVerifier},
+                { "grant_type","authorization_code"},
+                {"redirect_uri",redirectUrl }
+            };
+            var tokenResult = await HttpClientHelper.SendPostRequest<TokenResult>(tokenEndpoint, oauthParams);
+            var refreshedToken = await GoogleOauthService.RefreshTokenAsync(tokenResult.RefreshToken);
+            return tokenResult;
+        }
+        public static async Task<TokenResult> RefreshTokenAsync(string refreshToken)
+        {
+            var refreshParams = new Dictionary<string, string>
+            {
+                { "client_id", _clientId },
+                { "client_secret", _clientSecret },
+                { "grant_type", "refresh_token" },
+                { "refresh_token", refreshToken }
+            };
+
+            var tokenResult = await HttpClientHelper.SendPostRequest<TokenResult>(_refreshEndpoint, refreshParams);
+
+            return tokenResult;
+        }
+    }
+}
